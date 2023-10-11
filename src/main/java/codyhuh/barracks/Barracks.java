decbd9690974f10706a15d1d4ecf3a8a9ceae146
Coda <cody.koelliker@outlook.com>
2023-10-10 21:44:53 -0600

    fix spindly gauntlet with ash's help

--- src/main/java/codyhuh/barracks/Barracks.java
+++ src/main/java/codyhuh/barracks/Barracks.java
@@ -3,13 +3,24 @@ package codyhuh.barracks;
 import codyhuh.barracks.registry.ModEntities;
 import codyhuh.barracks.registry.ModItems;
 import codyhuh.barracks.registry.ModMenus;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 @Mod(Barracks.MOD_ID)
 public class Barracks {
@@ -31,5 +42,13 @@ public class Barracks {
                 e.accept(item.get());
             }
         }
+    }
+
+    public static EntityHitResult getLookAtEntity(Player player, Level level, double range) {
+        Vec3 eyePos = player.getEyePosition(1.0F);
+        Vec3 viewVec = player.getViewVector(1.0F);
+        Vec3 endVec = eyePos.add(viewVec.x * range, viewVec.y * range, viewVec.z * range);
+
+        return ProjectileUtil.getEntityHitResult(level, player, eyePos, endVec, player.getBoundingBox().inflate(range), e -> e instanceof LivingEntity);
     }
 }
--- src/main/java/codyhuh/barracks/common/items/SpindlyGauntletItem.java
+++ src/main/java/codyhuh/barracks/common/items/SpindlyGauntletItem.java
@@ -1,22 +1,16 @@
 package codyhuh.barracks.common.items;
 
-import com.google.common.collect.Maps;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
+import codyhuh.barracks.Barracks;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Vanishable;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
-
-import java.util.*;
-import java.util.stream.Collectors;
+import net.minecraft.world.phys.EntityHitResult;
 
 public class SpindlyGauntletItem extends Item implements Vanishable {
 
@@ -26,26 +20,15 @@ public class SpindlyGauntletItem extends Item implements Vanishable {
 
     @Override
     public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean held) {
-        super.inventoryTick(stack, level, entity, slot, held);
-
         if (held && entity instanceof Player player) {
-            List<LivingEntity> list = level.getNearbyEntities(LivingEntity.class, TargetingConditions.forNonCombat(), player, player.getBoundingBox().expandTowards(player.getViewVector(1.0F).multiply(128.0F, 128.0F, 128.0F)));
+            EntityHitResult result = Barracks.getLookAtEntity(player, level, 64.0D);
 
-            if (list.isEmpty()) return;
+            if (result != null && result.getEntity() instanceof LivingEntity living) {
+                living.hurt(living.damageSources().generic(), 5.0F);
+                living.addEffect(new MobEffectInstance(MobEffects.GLOWING, 2, 0, true, false, false));
 
-            LivingEntity nearestEntity = list.stream().min(Comparator.comparingDouble(e -> e.distanceToSqr(player))).get();
-
-            if (lookAtAmount(player, nearestEntity) > 0.999D) {
-                nearestEntity.addEffect(new MobEffectInstance(MobEffects.GLOWING, 2));
             }
         }
-    }
-
-    private double lookAtAmount(Player player, LivingEntity target) {
-        Vec3 vec3 = player.getViewVector(1.0F).normalize();
-        Vec3 vec31 = new Vec3(target.getX() - player.getX(), target.getEyeY() - player.getEyeY(), target.getZ() - player.getZ());
-        vec31 = vec31.normalize();
-        return vec3.dot(vec31);
     }
 
 }
